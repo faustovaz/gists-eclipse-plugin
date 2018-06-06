9154d694e0ecd038e7f2bb31710572a87baafda8
Jefferson Fausto Vaz <faustovaz@gmail.com>
2018-06-06 15:17:14 -0300

    Adding content provider to the treeview

---
+++ plugin.github.gists.core/src/main/java/me/faustovaz/plugin/github/gists/view/provider/GistCellLabelProvider.java
@@ -0,0 +1,14 @@
+package me.faustovaz.plugin.github.gists.view.provider;
+
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.ViewerCell;
+
+public class GistCellLabelProvider extends CellLabelProvider {
+
+    @Override
+    public void update(ViewerCell viewCell) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
