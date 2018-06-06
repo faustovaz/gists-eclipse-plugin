7d3c772aba752b97b658a00ea48eea18c00bcbfa
Jefferson Fausto Vaz <faustovaz@gmail.com>
2018-06-06 15:17:06 -0300

    Adding content provider to the treeview

--- plugin.github.gists.core/src/main/java/me/faustovaz/plugin/github/gists/view/provider/GistContentProvider.java
+++ plugin.github.gists.core/src/main/java/me/faustovaz/plugin/github/gists/view/provider/GistContentProvider.java
@@ -1,14 +1,38 @@
 package me.faustovaz.plugin.github.gists.view.provider;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.egit.github.core.Gist;
+import org.eclipse.egit.github.core.GistFile;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 
-public class GistContentProvider implements IStructuredContentProvider {
+public class GistContentProvider implements ITreeContentProvider {
     
     @Override
     public Object[] getElements(Object inputElement) {
         return ((List<?>) inputElement).toArray();
+    }
+
+    @Override
+    public Object[] getChildren(Object obj) {
+        Gist g = new Gist();
+        g.setDescription("UHEEET");
+        g.setFiles(new HashMap<>());
+        return Arrays.asList(g).toArray();
+    }
+
+    @Override
+    public Object getParent(Object arg0) {
+        return null;
+    }
+
+    @Override
+    public boolean hasChildren(Object obj) {
+        Map<String, GistFile> files = ((Gist) obj).getFiles();
+        return files.size() > 1;
     }
     
 }
